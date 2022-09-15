4f39c1ddf1ac02586fbe164e708ad495479560a7
Sam Pierson <sam@geometer.io>
2022-09-15 14:04:57 -0800

    server: server.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const jsonwebtoken = require('jsonwebtoken');
+
+const buildFolder = '../client/build';
+const { VEX_API_KEY_ID, VEX_API_SECRET, VEX_ROOM_ID } = process.env;
+
+function createJWT(roomId) {
+  const payload = {
+    "action": "join",
+    "room_id": roomId
+  }
+
+  return jsonwebtoken.sign(
+    payload,
+    VEX_API_SECRET,
+    {
+      header: { "api_key_id": VEX_API_KEY_ID }
+    }
+  )
+}
+
+const app = express();
+app.set('views', path.join(__dirname, buildFolder));
+app.engine('html', require('ejs').renderFile);
+
+app.use('/static', express.static(path.join(__dirname, `${buildFolder}/static`)));
+
+app.get('/', function (req, res) {
+  const jwt = createJWT(VEX_ROOM_ID);
+  res.render('index.html', { VEX_ROOM_ID, JWT: jwt });
+});
+
+app.listen(4000, () => console.log(`Server listening on port 4000`));
